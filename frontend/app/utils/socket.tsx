b4946ba93f63ba8e8e2f6ef7f62520b17e783fbe
arynsharma01 <156318218+arynsharma01@users.noreply.github.com>
2025-04-15 13:29:24 +0530

    Update socket.tsx

--- frontend/app/utils/socket.tsx
+++ frontend/app/utils/socket.tsx
@@ -1,24 +1,22 @@
 import { io, Socket } from "socket.io-client";
 
-let socket: Socket | null = null
+let socket: Socket | null = null;
 
 export const getSocket = (): Socket | null => {
-    if (typeof window === "undefined") return null        
-        if (!socket) {
-            const token = localStorage.getItem('Authorization')
-            if (!token) return null
+  
+  if (typeof window === "undefined") return null;
 
-            socket =
-                io("https://luckroll-production.up.railway.app", {
-                    auth: {
-                        token: token
-                    },
-                    autoConnect : true
-                })
-            
-        }
-        return socket
+  if (!socket) {
+    const token = localStorage.getItem("Authorization")
+    if (!token) return null;
 
+    socket = io("https://luckroll-production.up.railway.app", {
+      auth: {
+        token: token,
+      },
+      autoConnect: true,
+    });
+  }
+
+  return socket
 }
-
-
